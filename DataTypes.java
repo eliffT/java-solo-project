ce918fa451808e0ead47dff54af71e6fb99584b8
eliffT <elifftasdemirr@gmail.com>
2025-05-24 14:46:23 +0300

    [JA-2] - Java'da Değişkenler ve Veri Tipleri

---
+++ DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+
+        byte no = 100;
+        int number = 13443;
+        double decimall = 3.14159;
+        float smallDecimal = 2.5F;
+
+        boolean isTrue = false;
+        char letter = 'E';
+        String message = "JA-2 - Variables and Data Types in Java";
+
+        // const variable
+        final double PI=3.14159;
+
+        System.out.println("Byte variable: " + no);
+        System.out.println("int variable: " + number);
+        System.out.println("double variable: " + decimall);
+        System.out.println("float variable: " + smallDecimal);
+        System.out.println("boolean variable: " + isTrue);
+        System.out.println("char variable: " + letter);
+        System.out.println("String variable: " + message);
+        System.out.println("final (const) PI variable: " + PI);
+    }
+
+}
